2a5a222d5045ab180af12908804af4063354e2aa
Nguyễn Thành Sứ <tsu14758@gmail.com>
2025-03-19 21:58:43 +0700

    Fix merger conslicts

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
-<<<<<<< HEAD
 
 Route::get('/', function () {
     return view('pages.home');
@@ -15,23 +14,25 @@ Route::get('/', function () {
 // Route::get('/Home', function () {
 //     return view('pages.index');
 // });
-=======
 
 // Route::get('/', function () {
 //     return view('home');
 // });
 
-Route::get('/', function () {
-    return view('index');
-});
+// Route::get('/', function () {
+//     return view('index');
+// });
 
-Route::get('/shop', [ProductController::class, 'index'])->name('shop');
+// Route::get('/shop', [ProductController::class, 'index'])->name('shop');
 
-Route::get('/Home', function () {
-    return view('pages.index');
-});
+// Route::get('/Home', function () {
+//     return view('pages.index');
+// });
 
-Route::get('/login', function () {
-    return view('pages.login');
-});
->>>>>>> 1107accedf32bd087465e611aa6fd5ed3e1ace1d
+// Route::get('/login', function () {
+//     return view('pages.login');
+// });
+
+Route::get('/contact', function () {
+    return view('pages.contact');
+})->name('contact');

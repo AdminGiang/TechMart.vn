e8efe8c3055160b6053174eeb9c5dc6f74a17117
ThanhSu <tsu14758@gmail.com>
2025-03-20 16:43:28 +0700

    out

--- routes/web.php
+++ routes/web.php
@@ -1,57 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 Route::get('/', [ProductController::class, 'home'])->name('home'); 
 
 Route::get('/contact', function () {
     return view('pages.contact');
 })->name('contact');
-<<<<<<< HEAD
-
-Route::get('/blog', function () {
-    return view('pages.blog');
-})->name('blog');
-
-Route::get('/product', function () {
-    return view('pages.product');
-})->name('product');
-
-Route::get('/productdetail', function () {
-    return view('pages.productdetail');
-})->name('productdetail');
-
-
-
-// Route::get('/shop', [ProductController::class, 'index'])->name('shop');
-
-// Route::get('/Home', function () {
-//     return view('pages.index');
-// });
-
-// Route::get('/', function () {
-//     return view('home');
-// });
-
-// Route::get('/', function () {
-//     return view('index');
-// });
-
-// Route::get('/shop', [ProductController::class, 'index'])->name('shop');
-
-// Route::get('/Home', function () {
-//     return view('pages.index');
-// });
-
-// Route::get('/login', function () {
-//     return view('pages.login');
-// });
-
-Route::get('/checkout', function () {
-    return view('pages.checkout');
-})->name('checkout');
-
-=======
 
 Route::get('/blog', function () {
     return view('pages.blog');
@@ -77,8 +33,3 @@ Route::get('/productdetail', function () {
 Route::get('/cart', function () {
     return view('pages.cart');
 })->name('cart');
-
-
-
-
->>>>>>> 207589b054b5ab8fb412005e15352a82bec3e38a

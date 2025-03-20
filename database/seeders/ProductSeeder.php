d7f60cc2b575c0729e28d58c7c73a9b73c71fc5a
TG <truonggiangvu231023@gmail.com>
2025-03-20 17:04:38 +0700

    hoàn tất lấy sản phẩm từ database xuống

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -11,8 +11,22 @@ class ProductSeeder extends Seeder
     public function run(): void
     {
         DB::table('products')->insert([
+            
             [
                 'name' => 'Iphone 16 Pro Max',
+                'description' => 'Điện thoại Iphone 16 Pro Max',
+                'categoryId' => 2,
+                'userId' => 1,
+                'price' => 30000000,
+                'isPublish' => 1,
+                'sale' => 10,
+                'image' => 'iphone.jpg',
+                'tag' => 'điện thoại, iphone, 16 pro max',
+                'isTagBlog' => 0,
+                'count' => 10
+            ],
+            [
+                'name' => 'Iphone 15 Pro Max',
                 'description' => 'Điện thoại Iphone 16 Pro Max',
                 'categoryId' => 2,
                 'userId' => 1,
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -40,9 +40,9 @@
 					 @foreach($products as $product)					
 					<div class="col-12 col-md-4 col-lg-3 mb-5">
 						<a class="product-item" href="{{ route('productdetail')}}">
-							<img src="{{asset('assets/images/product-3.png')}}" class="img-fluid product-thumbnail">
-							<h3 class="product-title">{{ $product->name }}</h3>
-							<strong class="product-price">{{ $product->price }}</strong>
+							<img src="{{$product->Image}}"  class="img-fluid product-thumbnail">
+							<h3 class="product-title">{{ $product->Name }}</h3>
+							<strong class="product-price">{{ $product->Price }}</strong>
 					
 							<span class="icon-cross">
 								<img src="{{asset('assets/images/cross.svg')}}" class="img-fluid">

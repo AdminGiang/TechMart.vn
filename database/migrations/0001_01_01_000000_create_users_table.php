a097380cbb3137f1b1f63fa8218de7cd0a4fa3c5
TG <truonggiangvu231023@gmail.com>
2025-03-24 15:13:57 +0700

    migrate

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('phonenumber')->nullable();
             $table->text('address')->nullable();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->timestamp('email_verified_at')->nullable(); // Xác thực email
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
